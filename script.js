0fc781b9106517a61eace092ea8b4a78b9c38f3a
Lena Pavlenko <elpav1995@gmail.com>
2021-11-25 14:35:54 +0300

    Сделано первое ДЗ

---
+++ script.js
@@ -0,0 +1,13 @@
+// Создание переменных
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+// Вывод сообщения в alert
+alert('Hello, world');
+
+// Вывод сообщения в консоль
+console.log('Hi, I am a noob in JS');
