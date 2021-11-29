70a8ca5876b179e6ef7094d62e07afa1d17398b4
Lena Pavlenko <elpav1995@gmail.com>
2021-11-29 15:23:49 +0300

    feat(script.js): new feat

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-// Создание переменных
-const title = 'jsCourse';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 500;
+'use strict';
+
+// Запрашивание у пользователя основных данных
+const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?');
+const screenPrice = +prompt('Сколько будет стоить данная работа?');
+const adaptive = confirm('Нужен ли адаптив на сайте?');
+
+// Запрашивание дополнительной информации
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = +prompt('Сколько это будет стоить?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = +prompt('Сколько это будет стоить?');
+
+// Вычисление итоговой стоимости
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+// Вычисление стоимости с учетом отката
 const rollback = 24;
-const fullPrice = 30000;
-const adaptive = true;
+const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)) );
+console.log(servicePercentPrice);
+
+// Конструкция условий
+if (fullPrice >= 30000) {
+    console.log('Даем скидку в 10%');
+} else if (fullPrice > 15000 && fullPrice < 30000) {
+    console.log('Даем скидку в 5%');
+} else if (fullPrice <= 15000 && fullPrice > 0) {
+    console.log('Скидка не предусмотрена');
+} else if (fullPrice <= 0) {
+    console.log('Что-то пошло не так');
+}
+
 
 // Вывод типа переменных в консоль
 console.log(typeof title);
