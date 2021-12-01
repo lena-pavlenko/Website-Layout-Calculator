d7da0816ac48cbb37b3aed403e1a71e44a188244
Lena Pavlenko <elpav1995@gmail.com>
2021-12-01 19:07:47 +0300

    feat(script.js): new feat

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 'use strict';
 
 // Объявление переменных
-const title = prompt('Как называется ваш проект?');
-const screens = prompt('Какие типы экранов нужно разработать?');
-const screenPrice = +prompt('Сколько будет стоить данная работа?');
-const adaptive = confirm('Нужен ли адаптив на сайте?');
-const service1 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice1 = +prompt('Сколько это будет стоить?');
-const service2 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice2 = +prompt('Сколько это будет стоить?');
-const rollback = 24;
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+let rollback = 24;
+let service1;
+let service2;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
 
-// Объявление функции для определения типа переменной
+// Функция для проверки на число
+const isNumber = function(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+}
+
+// Функция для вызова основных вопросов
+const asking = function() {
+    title = prompt('Как называется ваш проект?');
+    screens = prompt('Какие типы экранов нужно разработать?');
+
+    // Пока не введено число, вопрос будет задавать снова
+    do {
+        screenPrice = prompt('Сколько будет стоить данная работа?');
+    } while (!isNumber(screenPrice));
+
+    screenPrice = +screenPrice; // Переводим ответ пользователя в числовой тип (убираем пробелы) и присваиваем переменной
+
+    adaptive = confirm('Нужен ли адаптив на сайте?');
+}
+
+// Функция для определения типа переменной
 const showTypeOf = function(variable) {
     console.log(variable, typeof variable);
 }
-// Объявление функции, определяющей сообщение о скидке
+
+// Функция, определяющей сообщение о скидке
 const getRollbackMessage = function(price) {
     if (price >= 30000) {
         return 'Даем скидку в 10%';
@@ -30,28 +52,53 @@ const getRollbackMessage = function(price) {
         return 'Что-то пошло не так';
     }
 }
-// Объявление функции для подсчета суммы доп.услуг
-const getAllServicePrices = function(extra1, extra2) {
-    return extra1 + extra2;
+
+// Функция для подсчета суммы доп.услуг
+const getAllServicePrices = function() {
+    let sum = 0;
+    let servicePrice;
+
+    // Цикл для вывода вопросов
+    for (let i = 0; i < 2; i++) {
+        if (i === 0) {
+           service1 = prompt('Какой дополнительный тип услуги нужен?');
+        }
+        if (i === 1) {
+           service2 = prompt('Какой дополнительный тип услуги нужен?');
+        }
+
+        // Пока не введено число, задаем вопрос снова
+        do {
+            servicePrice = prompt('Сколько это будет стоить?');
+        } while (!isNumber(servicePrice));
+        
+        sum += +servicePrice; // Складываем ответы пользователя
+        
+    }
+    return sum;
 }
-// Объявление функции для подсчета полной стоимости
+
+// Функция для подсчета полной стоимости
 function getFullPrice(myScreenPrice, allPrice) {
     return myScreenPrice + allPrice;
 }
-// Объявление функции для форматирования текста заголовка
+
+// Функция для форматирования текста заголовка
 const getTitle = function(myTitle) {
     myTitle = myTitle.trim();
     return myTitle[0].toUpperCase() + myTitle.slice(1).toLowerCase();
 }
-// Объявление функции для подсчета стоимости с учетом отката
+
+// Функция для подсчета стоимости с учетом отката
 const getServicePercentPrices = function(myFullPrice, myRollback) {
     return Math.ceil(myFullPrice - (myFullPrice * (myRollback / 100)) );
 }
 
 // Функциональный блок
-const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2); // Узнаем сумму за доп.услуги
-const fullPrice = getFullPrice(screenPrice, allServicePrices);              // Узнаем полную стоимость
-const servicePercentPrice = getServicePercentPrices(fullPrice, rollback);   // Узнаем стоимость с учетом отката
+asking(); // Выводим вопросы
+allServicePrices = getAllServicePrices(); // Узнаем сумму за доп.услуги
+fullPrice = getFullPrice(screenPrice, allServicePrices);              // Узнаем полную стоимость
+servicePercentPrice = getServicePercentPrices(fullPrice, rollback);   // Узнаем стоимость с учетом отката
 
 // Вывод типа переменных
 showTypeOf(title);
